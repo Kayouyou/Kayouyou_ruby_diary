65a2762719cf07b35bab557d018e288a237de190
Mark <18929562811@163.com>
2017-10-19 21:45:33 +0800

    循环语句

---
+++ loop.rb
@@ -0,0 +1,14 @@
+# 循环
+
+input = gets.chomp.to_i # 去除空格转变为数字
+
+while input != 0
+	puts("hahahhahaha")
+	break if input == 2
+	next  if input == 3
+	puts("heheheheheheh")
+end
+
+# until input != 2
+# 	puts("heheheheheheh")
+# end
