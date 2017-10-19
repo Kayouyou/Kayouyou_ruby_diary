ce89f96b0b46b839358fb78c158c4bd8a3c155d0
Mark <18929562811@163.com>
2017-10-19 21:32:58 +0800

    ruby的基本控制流语法

--- input.rb
+++ input.rb
@@ -1,6 +1,17 @@
 # ruby的基础语法
-input = gets.to_i
+input = gets.to_i 
 
+# 控制流
 if input == 2
 	puts("这里判等了")
+	system "say 妈妈问我怎么不开心？" # 10以上系统支持中文
+else
+	system "say 我想要一台MAC电脑"
+end
+
+# 跟if是相反的用法
+unless input == 100
+	puts("100")
+else
+	puts("001")
 end
