ab9288abcdf068cfbb8fd2818e71ef63dc597d06
Mark <18929562811@163.com>
2017-10-20 22:32:53 +0800

    ruby 的方法 以及带默然参数的方法的调用

--- error.rb
+++ error.rb
@@ -1,10 +1,21 @@
 # 异常的错误的处理,
 a = 100
-while true
-	b = gets.to_i
-	begin
-		puts(a/b)
-	rescue Exception => e
-		puts("输入不能为零o！！！")
-	end
+# while true
+# 	b = gets.to_i
+# 	begin
+# 		puts(a/b)
+# 	rescue Exception => e
+# 		puts("输入不能为零o！！！")
+# 	end
+# end
+
+# 方法
+def test(a=3,b=2)
+	puts(a + b)
+	return a + b
+
 end
+
+test 12,20
+
+
